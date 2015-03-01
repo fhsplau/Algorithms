8c27e3eb19d48d57413d8f90533f9cbcd94ab7b9
“Piotr <piotr.kacprzak11@gmail.com>
2015-03-01 16:47:47 +0000

    insertion sort first implementation

--- src/main/scala/io/paverell/algorithms/sort/InsertionSort.scala
+++ src/main/scala/io/paverell/algorithms/sort/InsertionSort.scala
@@ -1,7 +1,24 @@
 package io.paverell.algorithms.sort
 
+import scala.collection.mutable.ListBuffer
+
 class InsertionSort {
 
-  def sort(l: List[Int]): List[Int] = if (l.isEmpty) List() else null
+  def sort(l: List[Int]): List[Int] = {
+
+    val tmpL: ListBuffer[Int] = ListBuffer(l: _*)
+
+    for (x <- 0 to tmpL.size - 1) {
+      for (y <- 1 to tmpL.size - 1) {
+        if (tmpL(y - 1) > tmpL(y)) {
+          val tmp = tmpL(y - 1)
+          tmpL(y - 1) = tmpL(y)
+          tmpL(y) = tmp
+        }
+      }
+    }
+
+    List(tmpL: _*)
+  }
 
 }
--- src/test/scala/io/paverell/algorithms/sort/InsertionSortTest.scala
+++ src/test/scala/io/paverell/algorithms/sort/InsertionSortTest.scala
@@ -15,4 +15,12 @@ class InsertionSortTest extends FunSuite with BeforeAndAfter {
   test("if list is empty sort returns empty list") {
     assert(is.sort(List()) === List())
   }
+
+  test("sort non empty list") {
+    assert(is.sort(List(6,4,1)) === List(1,4,6))
+  }
+
+  test("should return sorted list if input is sorted") {
+    assert(is.sort(List(1,2,3))=== List(1,2,3))
+  }
 }
