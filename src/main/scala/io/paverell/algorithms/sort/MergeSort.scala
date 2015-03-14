efdf1d25dc85fb2cce9840e6437e7d0b59ad9358
“Piotr <piotr.kacprzak11@gmail.com>
2015-03-14 18:25:10 +0000

    list was changed to a ArrayBuffer

--- src/main/scala/io/paverell/algorithms/sort/MergeSort.scala
+++ src/main/scala/io/paverell/algorithms/sort/MergeSort.scala
@@ -1,48 +1,57 @@
 package io.paverell.algorithms.sort
 
+import scala.collection.mutable.ArrayBuffer
+
 class MergeSort {
 
-  def sort(list: List[Int]): List[Int] = {
+  def sort(list: ArrayBuffer[Int]): ArrayBuffer[Int] = {
     val listSize = list.size
 
-    def merge(l: List[Int], r: List[Int]): List[Int] = {
+    def merge(l: ArrayBuffer[Int], r: ArrayBuffer[Int], listToMerge: ArrayBuffer[Int]): ArrayBuffer[Int] = {
       var i = 0
       var j = 0
-      var merged: List[Int] = List()
+      var k = 0
 
       while(i < l.size && j < r.size) {
         if(l(i) < r(j)){
-          merged = merged ::: List(l(i))
+          listToMerge(k) = l(i)
           i += 1
         } else {
-          merged = merged ::: List(r(j))
+          listToMerge(k) = r(j)
           j += 1
         }
+        k += 1
       }
 
       while(i < l.size) {
-        merged = merged ::: List(l(i))
+        listToMerge(k) = l(i)
         i += 1
+        k += 1
       }
 
       while(j < r.size) {
-        merged = merged ::: List(r(j))
+        listToMerge(k) = r(j)
         j += 1
+        k += 1
       }
 
-      merged
+      listToMerge
     }
 
-    def split(l: List[Int], r: List[Int]): List[Int] = {
-      if(l.size < 2 && r.size < 2) merge(l, r)
+    def split(l: ArrayBuffer[Int], r: ArrayBuffer[Int], listToMerge: ArrayBuffer[Int]): ArrayBuffer[Int] = {
+      if(l.size < 2 && r.size < 2) merge(l, r, listToMerge)
       else{
-        merge(split(l take l.size/2, l drop l.size/2), split(r take r.size/2, r drop r.size/2))
+        merge(
+          split(l take l.size/2, l drop l.size/2, l),
+          split(r take r.size/2, r drop r.size/2, r),
+          listToMerge
+        )
       }
 
     }
 
     if(listSize <= 1) list
-    else split(list take listSize/2, list drop listSize/2)
+    else split(list take listSize/2, list drop listSize/2, list)
   }
 
 }
--- src/test/scala/io/paverell/algorithms/sort/MergeSortTest.scala
+++ src/test/scala/io/paverell/algorithms/sort/MergeSortTest.scala
@@ -5,6 +5,8 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.scalatest.junit.JUnitRunner
 import scala.util.Random
 
+import scala.collection.mutable.ArrayBuffer
+
 @RunWith(classOf[JUnitRunner])
 class MergeSortTest extends FunSuite with BeforeAndAfter{
   var ms: MergeSort = _
@@ -14,27 +16,27 @@ class MergeSortTest extends FunSuite with BeforeAndAfter{
   }
 
   test("if list is empty sort returns empty list") {
-    assert(ms.sort(List()) === List())
+    assert(ms.sort(ArrayBuffer()) === ArrayBuffer())
   }
 
   test("sort non empty list") {
-    assert(ms.sort(List(1)) === List(1))
-    assert(ms.sort(List(2,1)) === List(1,2))
-    assert(ms.sort(List(6, 4, 1)) === List(1, 4, 6))
+    assert(ms.sort(ArrayBuffer(1)) === ArrayBuffer(1))
+    assert(ms.sort(ArrayBuffer(2,1)) === ArrayBuffer(1,2))
+    assert(ms.sort(ArrayBuffer(6, 4, 1)) === ArrayBuffer(1, 4, 6))
   }
 
   test("should return sorted list if input is sorted") {
-    assert(ms.sort(List(1, 2, 3)) === List(1, 2, 3))
+    assert(ms.sort(ArrayBuffer(1, 2, 3)) === ArrayBuffer(1, 2, 3))
   }
 
   test("duplicates in the list") {
-    assert(ms.sort(List(6, 4, 1, 6, 2, 1)) === List(1, 1, 2, 4, 6, 6))
+    assert(ms.sort(ArrayBuffer(6, 4, 1, 6, 2, 1)) === ArrayBuffer(1, 1, 2, 4, 6, 6))
   }
 
   test("huge array") {
-    val n = 10000
+    val n = 1000000
     val a: Seq[Int] = 1 to n map { _ => Random.nextInt(n-1)}
-    val array: List[Int] = List(a: _*)
+    val array: ArrayBuffer[Int] = ArrayBuffer(a: _*)
 
     assert(ms.sort(array) === array.sorted)
   }
