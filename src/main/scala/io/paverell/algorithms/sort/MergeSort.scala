8413b0c853500c65b1507efcbd0547c2faeaf7dd
“Piotr <piotr.kacprzak11@gmail.com>
2015-03-14 18:41:40 +0000

    small refactoring

--- src/main/scala/io/paverell/algorithms/sort/MergeSort.scala
+++ src/main/scala/io/paverell/algorithms/sort/MergeSort.scala
@@ -5,53 +5,55 @@ import scala.collection.mutable.ArrayBuffer
 class MergeSort {
 
   def sort(list: ArrayBuffer[Int]): ArrayBuffer[Int] = {
-    val listSize = list.size
-
     def merge(l: ArrayBuffer[Int], r: ArrayBuffer[Int], listToMerge: ArrayBuffer[Int]): ArrayBuffer[Int] = {
       var i = 0
       var j = 0
       var k = 0
 
-      while(i < l.size && j < r.size) {
-        if(l(i) < r(j)){
+      def updateIndexes(ix: Int, jx: Int, kx: Int): Unit = {
+        i += ix
+        j += jx
+        k += kx
+      }
+
+      while (i < l.size && j < r.size) {
+        if (l(i) < r(j)) {
           listToMerge(k) = l(i)
-          i += 1
+          updateIndexes(1, 0, 0)
         } else {
           listToMerge(k) = r(j)
-          j += 1
+          updateIndexes(0, 1, 0)
         }
-        k += 1
+        updateIndexes(0, 0, 1)
       }
 
-      while(i < l.size) {
+      while (i < l.size) {
         listToMerge(k) = l(i)
-        i += 1
-        k += 1
+        updateIndexes(1, 0, 1)
       }
 
-      while(j < r.size) {
+      while (j < r.size) {
         listToMerge(k) = r(j)
-        j += 1
-        k += 1
+        updateIndexes(0, 1, 1)
       }
 
       listToMerge
     }
 
-    def split(l: ArrayBuffer[Int], r: ArrayBuffer[Int], listToMerge: ArrayBuffer[Int]): ArrayBuffer[Int] = {
-      if(l.size < 2 && r.size < 2) merge(l, r, listToMerge)
-      else{
+    def sortImpl(l: ArrayBuffer[Int], r: ArrayBuffer[Int], listToMerge: ArrayBuffer[Int]): ArrayBuffer[Int] = {
+      if (l.size < 2 && r.size < 2) merge(l, r, listToMerge)
+      else {
         merge(
-          split(l take l.size/2, l drop l.size/2, l),
-          split(r take r.size/2, r drop r.size/2, r),
+          sortImpl(l take l.size / 2, l drop l.size / 2, l),
+          sortImpl(r take r.size / 2, r drop r.size / 2, r),
           listToMerge
         )
       }
 
     }
 
-    if(listSize <= 1) list
-    else split(list take listSize/2, list drop listSize/2, list)
+    if (list.size <= 1) list
+    else sortImpl(list take list.size / 2, list drop list.size / 2, list)
   }
 
 }
