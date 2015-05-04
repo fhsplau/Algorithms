040d71c2c5e1921b2c59a99f9b4d3ff7358c3cff
“Piotr <piotr.kacprzak11@gmail.com>
2015-05-04 12:28:39 +0100

    added remove

--- src/main/scala/io/paverell/datastructures/BST/BST.scala
+++ src/main/scala/io/paverell/datastructures/BST/BST.scala
@@ -1,22 +1,12 @@
 package io.paverell.datastructures.BST
 
-// TODO add constructor
-// TODO add Node to non-empty tree
-// TODO add Node to empty tree
-// TODO sort
-// TODO print => toString
-// TODO delete element
-// TODO merge trees => try to do something bether than O(n)
-// TODO change to funcional programming
-// TODO is balanced
-// TODO do balance
-// TODO get method => add keys => O(n) or bether
-// TODO flatt
 
 abstract class BSTree {
   def add(e: Int): BSTree
 
   def contains(e: Int): Boolean
+
+  def remove(e: Int): BSTree
 }
 
 class EmptyNode extends BSTree {
@@ -25,6 +15,7 @@ class EmptyNode extends BSTree {
 
   override def contains(element: Int): Boolean = false
 
+  override def remove(e: Int): BSTree = ???
 }
 
 class Node(e: Int, left: BSTree, right: BSTree) extends BSTree {
@@ -37,6 +28,8 @@ class Node(e: Int, left: BSTree, right: BSTree) extends BSTree {
     if (element > e) right.contains(element)
     else if (element < e) left.contains(element)
     else true
+
+  override def remove(e: Int): BSTree = ???
 }
 
 
