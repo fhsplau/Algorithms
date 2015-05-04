2e81dad4c42fc0f62cdaa8abc54d23b2b2505724
“Piotr <piotr.kacprzak11@gmail.com>
2015-05-04 23:16:04 +0100

    new methods

--- src/main/scala/io/paverell/datastructures/BST/BST.scala
+++ src/main/scala/io/paverell/datastructures/BST/BST.scala
@@ -7,6 +7,14 @@ abstract class BSTree {
   def contains(e: Int): Boolean
 
   def remove(e: Int): BSTree
+
+  def min: Int
+
+  def max: Int
+
+  def isLeaf: Boolean
+
+  def isEmpty: Boolean
 }
 
 class EmptyNode extends BSTree {
@@ -15,7 +23,17 @@ class EmptyNode extends BSTree {
 
   override def contains(element: Int): Boolean = false
 
-  override def remove(e: Int): BSTree = ???
+  override def remove(e: Int): BSTree = this
+
+  override def min: Int = ???
+
+  override def max: Int = ???
+
+  override def isLeaf: Boolean = true
+
+  override def isEmpty: Boolean = true
+
+  override def toString = "."
 }
 
 class Node(e: Int, left: BSTree, right: BSTree) extends BSTree {
@@ -29,9 +47,18 @@ class Node(e: Int, left: BSTree, right: BSTree) extends BSTree {
     else if (element < e) left.contains(element)
     else true
 
-  override def remove(e: Int): BSTree = ???
-}
+  override def remove(element: Int): BSTree = ???
 
+  override def min: Int = if (left.isEmpty) e else left.min
+
+  override def max: Int = if (right.isEmpty) e else right.max
+
+  override def isLeaf: Boolean = left.isEmpty && right.isEmpty
+
+  override def isEmpty: Boolean = false
+
+  override def toString = "{" + left + e + right + "}"
+}
 
 class BST {
 
