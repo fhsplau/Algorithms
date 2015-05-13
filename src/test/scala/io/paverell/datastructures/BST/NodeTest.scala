d19d39ef31936e4ab7aca7c9e7d876ecdef1af89
paverell <piotr.kacprzak11@gmail.com>
2015-05-13 18:37:18 +0100

    replaced assert with Matchers

--- src/test/scala/io/paverell/datastructures/BST/NodeTest.scala
+++ src/test/scala/io/paverell/datastructures/BST/NodeTest.scala
@@ -3,6 +3,7 @@ package io.paverell.datastructures.BST
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatest.Matchers._
 
 @RunWith(classOf[JUnitRunner])
 class NodeTest extends FunSuite with BeforeAndAfter {
@@ -18,63 +19,65 @@ class NodeTest extends FunSuite with BeforeAndAfter {
   }
 
   test("node contains element") {
-    assert(node.contains(10))
+    node.contains(10) shouldBe true
   }
 
   test("can remove from left subtree") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
     node = node.remove(8)
     assertContainsAll(List(10, 12, 6, 9, 13, 11), node)
-    assert(!node.contains(8))
+    node.contains(8) should not be true
   }
 
   test("can remove from right subtree") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
     node = node.remove(12)
     assertContainsAll(List(10, 13, 6, 9, 8, 11), node)
-    assert(!node.contains(12))
+    node.contains(12) should not be true
   }
 
   test("can remove a leaf") {
     node = node.remove(10)
-    assert(node.isEmpty)
+    node.isEmpty shouldBe true
   }
 
   test("can retrieve min value from node") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
-    assert(node.min === 6)
+    node.min shouldEqual 6
   }
 
   test("can retrieve max value from node") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
-    assert(node.max === 13)
+    node.max shouldEqual 13
   }
 
   test("node is a leaf if it don't has left and right subtree") {
-    assert(node.isLeaf)
+    node.isLeaf shouldBe true
   }
 
   test("node is not a leaf if it has left or right subtree") {
     node = node.add(12)
     val node2 = new NonEmptyNode(11, new NonEmptyNode(9, EmptyNode, EmptyNode), EmptyNode)
     assertContainsAll(List(10, 12), node)
-    assert(!node.isLeaf)
-    assert(!node2.isLeaf)
+    node.isLeaf should not be true
+    node2.isLeaf should not be true
   }
 
   test("can retrieve size from a node") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
-    assert(node.size == 7)
+    node.size shouldEqual 7
   }
 
   test("size of left and right subtrees is smaller than roots size") {
     node = addAll(List(8, 12, 6, 9, 13, 11), node)
-    assert(node.asInstanceOf[NonEmptyNode].left.size < node.size)
-    assert(node.asInstanceOf[NonEmptyNode].right.size < node.size)
+    getNonEmptyNode(node).right.size should be < node.size
+    getNonEmptyNode(node).left.size should be < node.size
   }
 
+  def getNonEmptyNode(node: Node): NonEmptyNode = node.asInstanceOf[NonEmptyNode]
+
   def assertContainsAll(elements: List[Int], node: Node): Unit =
-    assert(elements.map(x => node.contains(x)).reduceLeft(_ && _))
+    elements.map(x => node.contains(x)).reduceLeft(_ && _) shouldBe true
 
   def addAll(elements: List[Int], node: Node): Node = {
     if (elements.isEmpty) node else addAll(elements.tail, node.add(elements.head))
